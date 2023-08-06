7d90c9a794fbb4bd3a29c24325c9eace7cfa77ec
William Rice <billyrice12@live.com>
2023-08-05 22:41:57 -0500

    updated keybind for telescope keymap to be <leader>km

--- lua/wrice/core/keymaps.lua
+++ lua/wrice/core/keymaps.lua
@@ -40,7 +40,7 @@ keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in 
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
-keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps<cr>")  -- list keymapping
+keymap.set("n", "<leader>km", "<cmd>Telescope keymaps<cr>")  -- list keymapping
 
 -- telescope git commands
 keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>") -- list all git commits (use <cr> to checkout) ["gc" for git commits]
